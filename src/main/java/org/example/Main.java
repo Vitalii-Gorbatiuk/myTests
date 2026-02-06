ebf096bf3572e92e6c580304c083a937aee16f59
Vitalii.Gorbatiuk <Vitalii.Gorbatiuk@gmail.com>
2026-02-06 23:01:14 +0200

    Small cleanup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,19 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    //    private static Map<String, Group> groups = new HashMap<>();
 
     public static void main(String[] args) {
-        //        Map<String, List<Long>> groups = new HashMap<>();
         var groups = new Groups.Builder(-100L, 99L)
-//                .addGroup("A", new Group<>(0.0, 0.1))
                 .addGroup("A", 0.0, 0.1)
-//                .addGroup("B", new Group<>(0.1, 0.4))
                 .addGroup("B", 0.1, 0.4)
-//                .addGroup("C", new Group<>(0.4, 0.65))
                 .addGroup("C", 0.4, 0.65)
-//                .addGroup("D", new Group<>(0.65, 1.0))
                 .addGroup("D", 0.65, 1.0)
-//                .addGroup("E", new Group<>(null, 1.0))
                 .build();
         logger.info("Groups: {}", groups);
 
